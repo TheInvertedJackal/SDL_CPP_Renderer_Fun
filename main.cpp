6737e1953f0e9874acd570a7c24ae6d7660ac1f1
David-McIlveen <115719576+David-McIlveen@users.noreply.github.com>
2024-01-30 21:12:30 -0600

    Cool Colors 2

--- main.cpp
+++ main.cpp
@@ -57,17 +57,30 @@ int main(int argc, char* argv[]){
             double x_value = ((double) x) / WINDOW_SIZE_X;
             double r1 = 255 * offset(x, 1, PI / (WINDOW_SIZE_X * 2), frame_count / 100.0);
             double r2 = 255 * offset(x, 1, PI / (WINDOW_SIZE_X * 4), frame_count / -50.0);
-            Uint32 g = (Uint32) (255 * offset(x, 1, PI / (WINDOW_SIZE_X * 2), frame_count / -30.0));
-            Uint32 b = (Uint32) (255 * offset(x, 1, PI / (WINDOW_SIZE_X * 1.5), frame_count / 50.0));
+            double g1 = 255 * offset(x, 1, PI / (WINDOW_SIZE_X * 1), frame_count / -30.0);
+            double g2 = 255 * offset(x, 1, PI / (WINDOW_SIZE_X * 3), frame_count / 75.0);
+            double b1 = 255 * offset(x, 1, PI / (WINDOW_SIZE_X * 2), frame_count / -50.0);
+            double b2 = 255 * offset(x, 1, PI / (WINDOW_SIZE_X * 4), frame_count / -200.0);
+            double b3 = 255 * offset(x, 1, PI / (WINDOW_SIZE_X * 3), frame_count / 50.0);
             for (size_t y = 0; y < WINDOW_SIZE_Y; y++)
             {
                 double y_value = ((double) y) / WINDOW_SIZE_Y;
                 double tempR = r1 * y_value + r2 * y_value * y_value;
+                double tempG = g1 * y_value + g2 + y_value * .2;
+                double tempB = b1 * y_value + b2 * y_value * y_value + b3 * .9;
                 if(tempR > 255){
                     tempR = 255;
                 }
+                if(tempG > 255){
+                    tempG = 255;
+                }
+                if(tempB > 255){
+                    tempB = 255;
+                }
                 Uint32 r = (Uint32) tempR;
-                pixels[WINDOW_SIZE_X * y + x] = get_32bit_ARGB(255, r , g * y_value * y_value, b * y_value / 2);
+                Uint32 g = (Uint32) tempG;
+                Uint32 b = (Uint32) tempB;
+                pixels[WINDOW_SIZE_X * y + x] = get_32bit_ARGB(255, r , g, b);
             }
         }
 
